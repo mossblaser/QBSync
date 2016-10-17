acda23bf93e988ea272dfb7d8bc598a135a6e04e
Jonathan Heathcote <mail@jhnet.co.uk>
2016-10-17 23:00:12 +0100

    Initial commit with proof-of-concept implementation.
    Super ugly, barely functional but remarkably effective! One real-world trial
    suggests this should work nicely!

---
+++ README.md
@@ -0,0 +1,12 @@
+QBSync: A synchronised video player for lonely hearts
+=====================================================
+
+QBSync (pronounced 'cubie sync') is a thrown-together web application which
+allows two (or more) people to watch a HTML 5 video at the same time.
+
+It was built to allow me and my wife to watch TV together while I am working
+away from home while we use Skype (or similar) to chuckle along with whatever
+we're watching. As a consequence, this software does not strive to achieve much
+better than 100ms-or-so sync. Its primary goal, however, is to ensure that
+controls are shared and that things like buffering etc. do not lead to players
+drifting out of sync.
---
+++ test.php
@@ -0,0 +1,150 @@
+<?php
+
+$state_file_name = "./state.json";
+
+// Time-out clients who haven't connected in a while
+$MAX_POLL_INTERVAL = 5.0;
+
+// Slack to add before synchronised playback starts
+$PLAY_START_SLACK = 1.0;
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the state
+////////////////////////////////////////////////////////////////////////////////
+
+$state_file = fopen($state_file_name, "r");
+flock($state_file, LOCK_EX);
+
+// Set up initial default state if no state file exists or its empty
+if (!file_exists($state_file_name) || filesize($state_file_name) <= 2) {
+	file_put_contents($state_file_name, json_encode(array(
+		// URL of video to play
+		"video_url" => "http://example.com",
+		
+		// Should playback be triggered when all clients are ready?
+		"video_play_on_all_ready" => false,
+		
+		// Should the video be playing right now?
+		"video_playing" => false,
+		
+		// What time should the video be seeked to at state_change_time?
+		"video_time" => 0.0,
+		
+		// What time did the state last change
+		"state_change_time" => 0.0,
+		
+		// Client states (array from client ID to state)
+		"clients" => array(),
+	)));
+}
+
+// Read back state
+$server_state = json_decode(file_get_contents($state_file_name), true);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Update client state
+////////////////////////////////////////////////////////////////////////////////
+
+$client_id = $_POST['client_id'];
+
+// Retrieve client's state
+$new_client_state = json_decode($_POST["client_state"], true);
+
+// Record that a message was receieved from the client
+// XXX: Subtract 1ms to make sure it is in the past...
+$new_client_state["last_contact"] = microtime(true) - 0.001;
+
+// Update client state
+$old_client_state = $server_state["clients"][$client_id];
+$server_state["clients"][$client_id] = $new_client_state;
+
+// Filter clients who haven't been in contact in a while
+function is_client_fresh($client_state) {
+	global $MAX_POLL_INTERVAL;
+	$last_contact = microtime(true) - $client_state["last_contact"];
+	return($last_contact < $MAX_POLL_INTERVAL);
+}
+$server_state["clients"] = array_filter($server_state["clients"], "is_client_fresh");
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Update server/playback state
+////////////////////////////////////////////////////////////////////////////////
+
+// Add current time
+$server_state["time"] = microtime(true);
+
+
+// Update the state to cause the video to seek to the specified play time
+function seekTo($time) {
+	global $server_state;
+	
+	// Pause (playback is resumed by video_play_on_all_ready if required).
+	$server_state["video_playing"] = false;
+	
+	// Set time to seek to
+	$server_state["video_time"] = $time;
+	
+	// Indicate state change
+	$server_state["state_change_time"] = $server_state["time"];
+}
+
+
+// Execute the arriving command
+$command = json_decode($_POST["command"], true);
+switch ($command["command"]) {
+	case "pause":
+		$server_state["video_play_on_all_ready"] = false;
+		seekTo($command["time"]);
+		break;
+	
+	case "seek":
+		$server_state["video_play_on_all_ready"] = true;
+		seekTo($command["time"]);
+		break;
+}
+
+// Update play state should be set based on all clients having polled
+// since the state changed 
+$all_ready = true;
+foreach ($server_state["clients"] as $client_state) {
+	// Client should seen the latest state and should be ready
+	if (!($client_state["state_change_time"] == $server_state["state_change_time"] &&
+	      $client_state["ready"])) {
+		$all_ready = false;
+	}
+}
+if ($all_ready && $server_state["video_play_on_all_ready"]) {
+	// Work out how long in the future the play command must be sent for clients
+	// to play back in sync
+	$max_delay = 0.0;
+	foreach ($server_state["clients"] as $client_state) {
+		$this_delay = $client_state["poll_interval"] + $client_state["round_trip_time"];
+		$max_delay = max($max_delay, $this_delay);
+	}
+	
+	// Start play back shortly in the future
+	$server_state["video_playing"] = true;
+	$server_state["state_change_time"] = $server_state["time"] + $max_delay + $PLAY_START_SLACK;
+	
+	// Clear the flag
+	$server_state["video_play_on_all_ready"] = false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Store the new state
+////////////////////////////////////////////////////////////////////////////////
+
+// Store the state to disk
+file_put_contents($state_file_name, json_encode($server_state));
+
+// Send state to client
+echo(json_encode($server_state));
+
+// Unlock the file again
+flock($state_file, LOCK_UN);
+fclose($state_file);
+
+?>
